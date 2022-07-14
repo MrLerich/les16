650ba1f8e127b46230789b2aa71bed08977a3c93
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-07-14 17:27:27 +0300

    fixing + add README.md

--- main.py
+++ main.py
@@ -93,9 +93,69 @@ def get_user_by_id(user_id):
             mimetype="application/json"
         )
     elif request.method == "PUT":
+        update_universal(User, user_id, request.json)
+        #универсальным методом обновления выбираем модель по PK где обновляем и значения обновления
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=20,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_universal(User, user_id)
+        #универсальным удалением выбираем модельи что удаляем по PK
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
 
+@app.route("/orders/<int:user_id>", methods=['GET', 'PUT', 'DELETE'])
+def get_order_by_id(user_id):
+    if request.method == 'GET':
+        data = get_all_by_id(Order, user_id)
+        return app.response_class(
+            response=json.dumps(data, indent=4),
+            status=200,
+            mimetype="application/json"
+        )
+    elif request.method == "PUT":
+        update_universal(Order, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=20,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_universal(Order, user_id)
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
 
-
+@app.route("/offers/<int:user_id>", methods=['GET', 'PUT', 'DELETE'])
+def get_offer_by_id(user_id):
+    if request.method == 'GET':
+        data = get_all_by_id(Offer, user_id)
+        return app.response_class(
+            response=json.dumps(data, indent=4),
+            status=200,
+            mimetype="application/json"
+        )
+    elif request.method == "PUT":
+        update_universal(Offer, user_id, request.json)
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=20,
+            mimetype='application/json'
+        )
+    elif request.method == 'DELETE':
+        delete_universal(Offer, user_id)
+        return app.response_class(
+            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
+            status=200,
+            mimetype="application/json"
+        )
 if __name__ == '__main__':
     init_db()
     app.run(host='127.0.0.1', port=8080, debug=True)
